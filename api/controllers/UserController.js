f42650d82769cad7d891e315bb1ce2f0c793a447
Jesse Hixson <hixsonj@gmail.com>
2014-03-12 11:12:45 -0400

    Adds user model/controller.

---
+++ api/controllers/UserController.js
@@ -0,0 +1,14 @@
+/**
+ * UserController.js 
+ *
+ * @description ::
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+
+  new: function(req, res) {
+    res.view();
+  }
+	
+};
