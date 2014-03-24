195088558d0a4a45137f170fb222f1b2e3f32969
Jesse Hixson <hixsonj@gmail.com>
2014-03-23 22:12:26 -0400

    Adds redis session store cuz why not?

--- config/session.js
+++ config/session.js
@@ -20,7 +20,7 @@ module.exports.session = {
 
   // In production, uncomment the following lines to set up a shared redis session store
   // that can be shared across multiple Sails.js servers
-  // adapter: 'redis',
+  adapter: 'redis',
   //
   // The following values are optional, if no options are set a redis instance running
   // on localhost is expected.
@@ -32,6 +32,9 @@ module.exports.session = {
   // db: 0,
   // pass: <redis auth password>
   // prefix: 'sess:'
+  host: 'grideye.redistogo.com',
+  port: 9960,
+  url: process.env.REDISTOGO_URL,
 
 
   // Uncomment the following lines to use your Mongo adapter as a session store
